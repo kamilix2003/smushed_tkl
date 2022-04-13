d15970d4a7222da93d32c3b9f3b3d9e86ea386d8
kamilix2003 <kamaj2309@gmail.com>
2022-04-13 21:06:58 +0200

    start

---
+++ rules.mk
@@ -0,0 +1,10 @@
+# This file intentionally left blank
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = halfkay
